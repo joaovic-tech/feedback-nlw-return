825e57437781b0c7e5ba48ea9b0de513b05309e4
João <joaovictorca2004@gmail.com>
2022-05-10 15:21:50 -0300

    test new table message

---
+++ server/prisma/migrations/20220510180813_new_table_message/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "message" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "message" TEXT NOT NULL,
+    "time" TEXT NOT NULL
+);
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import { routes } from './routes';
 
+const PORT = process.env.PORT || 3333
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3333, () => {
-  console.log('HTTP server running!');
+app.listen(PORT, () => {
+  if(PORT == 3333){
+    console.log(`HTTP server running! dev ${PORT}`);
+  }
+  console.log(`HTTP server running! deploy`);
 });
