383e42c805ab3e316a89a1814100245424e1cfd5
João <joaovictorca2004@gmail.com>
2022-06-10 12:53:05 -0300

    adjust deploy 3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { routes } from './routes';
-const PORT = process.env.PORT || process.env.PORT_URL;
+const PORT = process.env.PORT || 3333;
 
 export const app = express();
 
