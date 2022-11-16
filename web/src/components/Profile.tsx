3a8974183de2481603581f7493d34309a1b119cc
João <joaovictorca2004@gmail.com>
2022-11-16 20:19:40 -0300

    correct github api name and image

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -1,23 +1,26 @@
-import { myGithubApi } from "../lib/myGithubIpi";
-let img_url: string;
-let username: string;
+import axios from "axios";
+const url = 'https://api.github.com/users/joaovic-tech';
 
-myGithubApi.then(({ avatar_url, name }) => {
-  img_url = avatar_url;
-  username = name;
-}).catch(e => {
-  console.log(e);
-})
+axios.get(url)
+  .then(response => {
+    const username = document.getElementById('username')!;
+    const img = document.getElementById('img-profile')! as HTMLImageElement;
+
+    username.innerHTML = response.data.name;
+    img.src = response.data.avatar_url;
+  })
+  .catch(error => console.log(error))
+
 
 export function Profile() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <div className="grid">
+      <div className="grid text-center">
         <div className="relative w-32 h-32 m-auto">
           <div className="animate-spin w-full h-full absolute top-0 left-0 bg-gradient-to-b from-transparent to-slate-900/50 rounded-full"></div>
-          <img src={img_url} alt="Image profile" className="w-full rounded-full" />
+          <img src='' id="img-profile" alt="Image profile" className="w-full rounded-full" />
         </div>
-        <a href="https://joaovictor-portfolio.vercel.app/" className="text-gray-200 text-lg hover:text-brand-500">{username}</a>
+        <a href="https://joaovictor-portfolio.vercel.app/" className="text-gray-200 text-lg hover:text-brand-500" id="username"></a>
         <p className="text-gray-400">Desafio da <a className="underline underline-offset-2 hover:text-brand-500" href="https://rocketseat.com.br">Rocketseat</a>: Feedback NLW RETURN</p>
       </div>
     </div>
